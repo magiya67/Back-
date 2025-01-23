a622173336afba4576343a315ec71e553a745f9f
magiya67 <151738190+magiya67@users.noreply.github.com>
2025-01-23 15:14:21 +1000

    Update settings.py

--- Task_6/gateway_serv/gateway_serv/settings.py
+++ Task_6/gateway_serv/gateway_serv/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bf1zkg@z3h0pzi3v@@y*^fv2kxn(u!2tl686xv^m1x+trp$uir'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
